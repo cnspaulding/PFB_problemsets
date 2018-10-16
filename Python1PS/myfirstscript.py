e769f8ff085e938f920719ff9dc05c8709df7bff
csp <csp@pfb02.cshl.edu>
2018-10-16 11:25:58 -0400

    adding first Python1PS

--- Python1PS/myfirstscript.py
+++ Python1PS/myfirstscript.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+import sys
 
-print('My Name: Caitlin')
+favorite_activity = 'hiking'
+favorite_animal = 'giraffe'
+
+print('My name: Caitlin')
+print('My favorite color: teal')
+print('My favorite activity:', favorite_activity)
+print('My favorite animal:', favorite_animal)
+
+
+
+
+
---
+++ Python1PS/mysecondscriptwithsys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import sys
+
+my_name = sys.argv[1]
+favorite_color = sys.argv[2]
+favorite_activity = sys.argv[3]
+favorite_animal = sys.argv[4]
+
+print('My name:' ,  my_name , '\n' , 'My favorite color:' , favorite_color , '\n' ,  'My favorite activity:' , favorite_activity , '\n' , 'My favorite animal:' , favorite_animal)
+
+
