e3fbc3248317f633880a69df959ea4b66b78d4fe
csp <csp@pfb02.cshl.edu>
2018-10-17 13:52:57 -0400

    updating scripts with comments

--- Python3PS/ps3q14.py
+++ Python3PS/ps3q14.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
+#here I want to look to see if/where there is an ecoR1 cut site in this DNA string.
+
 #dna = 'CCCGAATTCGGG'
 dna = 'GATGGGATTggggttttccccTCCCATGTGCTCAAGACTGGCGCTaaaaGttttGAGCTTCTCaaaaGTCTAGAGCCACCGTCCAGGGAGCAGGTAGCTGCTGGGCTCCggggACACTTTGCGTTCGGGCTGGGAGCGTGCTTTCCACGACGGTGACACGCTTCCCTGGATTGGCAGCCAGACTGCCTTCCGGGTCACTGCCATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGccccCTCTGAGTCAGGAAACAttttCAGACCTATGGAAACTACTTCCTGaaaaCAACGTTCTGTccccCTTGCCGTCCCAAGCAATGGATGATTTGATGCTGTccccGGACGATATTGAACAATGGTTCACTGAAGACCCAGGTCCAGATGAAGCTCCCAGAATTCGCCAGAGGCTGCTccccCCGTGGccccTGCACCAGCAGCTCCTACACCGGCGGccccTGCACCAGccccCTCCTGGccccTGTCATCTTCTGTCCCTTCCCAGaaaaCCTACCAGGGCAGCTACGGTTTCCGTCTGGGCTTCTTGCATTCTGGGACAGCCAAGTCTGTGACTTGCACGTACTccccTGCCCTCAACAAGATGttttGCCAACTGGCCAAGACCTGCCCTGTGCAGCTGTGGGTTGATTCCACAccccCGCCCGGCACCCGCGTCCGCGCCATGGCCATCTACAAGCAGTCACAGCACATGACGGAGGTTGTGAGGCGCTGccccCACCATGAGCGCT'
 
+#EcoR1 string is -GAATTC
+#here look to see if this exact sequence is in the DNA
+#identify the exact location (bp) in the sequence it is. Becasue python starts counting with 0, the first bp is actually 0 here but I would start with 1. So to get the actual bp it starts with, you have to add 1. Then I added 5 more bases to account for the 5 bases following that complete the site
 ecoR1 = dna.find('GAATTC')
+
 ecoR1_start = ecoR1 + 1
 ecoR1_end = ecoR1_start + 5
 
+
+#here I want to pull out the exact sequence from the above. 
 ecoR1_site = dna[ecoR1:ecoR1_end]
+
 
 print('ecoR1 start site:', ecoR1_start)
 print('ecoR1 end site:', ecoR1_end)
 print('ecoR1 site:', ecoR1_site)
 
+#identify the bp up and down stream of the REsite
+
+up_downstream = dna[390:407]
+print(up_downstream)
 
